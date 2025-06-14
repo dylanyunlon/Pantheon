605cb3986dcef0abf62a4d160fed157da4d6d087
dylanyunlon <dylanyunlong@gmail.com>
2025-06-14 18:23:58 +0800

    chore: _

--- src/renderer/src-main-window/views/test/Test.vue
+++ src/renderer/src-main-window/views/test/Test.vue
@@ -5,17 +5,6 @@
       <div style="margin-bottom: 12px">
         This page is reserved for testing scenarios, can only be seen in dev or .rabi mode.
       </div>
-      <ExternalLink href="https://www.google.com">Google</ExternalLink>
-      <div style="width: 100%; height: 1px; background: #fff4"></div>
-      <!-- Horizontal Collapse Transition -->
-      <HorizontalExpand :show>
-        <div>should show me?</div>
-      </HorizontalExpand>
-      <NButton @click="show = !show">Toggle</NButton>
-      <NIcon style="font-size: 32px">
-        <SpinningIcon />
-      </NIcon>
-      <!-- Premade Teams Color Presets -->
       <div class="colors-container">
         <div
           class="card"
@@ -33,41 +22,14 @@
           <div class="info">borderColor: {{ team.borderColor }}</div>
         </div>
       </div>
-      <!-- Markdown Rendering Test -->
-      <div class="markdown-text markdown-body" v-html="markdownHtmlText"></div>
     </NScrollbar>
   </div>
 </template>
 
 <script setup lang="ts">
-import SpinningIcon from '@renderer-shared/assets/icon/SpinningIcon.vue'
-import ExternalLink from '@renderer-shared/components/ExternalLink.vue'
-import HorizontalExpand from '@renderer-shared/components/HorizontalExpand.vue'
 import { PREMADE_TEAM_COLORS } from '@renderer-shared/components/ongoing-game-panel/ongoing-game-utils'
-import { useInstance } from '@renderer-shared/shards'
-import { LeagueClientRenderer } from '@renderer-shared/shards/league-client'
-import { markdownIt } from '@renderer-shared/utils/markdown'
-import { useTranslation } from 'i18next-vue'
-import { NButton, NIcon, NScrollbar, useMessage } from 'naive-ui'
-import { Component as ComponentC, computed, h, reactive, ref } from 'vue'
-
-import { useMarkdownTest } from './markdown-test'
-
-const lc = useInstance(LeagueClientRenderer)
-const { t } = useTranslation()
-
-const message = useMessage()
-const value = ref('1')
-
-const renderIcon = (icon: ComponentC) => {
-  return () => h(NIcon, null, () => h(icon))
-}
-
-const textT = useMarkdownTest()
-
-const markdownHtmlText = computed(() => {
-  return markdownIt.render(textT.value)
-})
+import { NScrollbar } from 'naive-ui'
+import { reactive, ref } from 'vue'
 
 const teams = reactive(PREMADE_TEAM_COLORS)
 
